e0191175a1ef072f71bbe4eec626f31da00b2fa7
juanros13 <juanros13@gmail.com>
2016-08-18 14:12:12 -0500

    Quitando patterns de url.py ya que fue deprecada

--- django_ac22/apps/avisos/urls.py
+++ django_ac22/apps/avisos/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from apps.avisos import views as avisos_view
 
--- django_ac22/apps/obras/urls.py
+++ django_ac22/apps/obras/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from apps.obras import views as obras_view
 
